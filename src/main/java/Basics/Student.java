c72b9fc3994b8aca5206dd199417a7bd28555245
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-01-15 08:28:03 +0000

    Currency Converter print added and added details regarding variable declaration

--- src/main/java/Basics/Student.java
+++ src/main/java/Basics/Student.java
@@ -10,12 +10,23 @@ package Basics;
  * @author HP
  */
 public class Student {
+    //Class and static variables are optional in declaration, int id; will also work fine
+    //default value of int will be 0. If the variable is undeclared, the default value is updates to the variable
+    int id = 1000;
+    /*int x;
+    x=10; //this declaration is not possible in java*/
+    
     public static void main(String[] args){
         Student s = new Student();
         s.compute();
     }
-    int id = 1000;
+
     void compute(){
+        //id = ""; statically bind language
+        //these local variables are compulsory to initialize
+        int nextID =id+1;
         System.out.println(id);
+        System.out.println(nextID);
+        System.out.println("The program ran succesfully");
     }    
 }
