2a82e8240a33ff3d88b40e9ce1ef85b4de485215
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-01-15 10:50:41 +0530

    initialised the folder

---
+++ src/main/java/Basics/Student.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basics;
+
+/**
+ *
+ * @author HP
+ */
+public class Student {
+    public static void main(String[] args){
+        Student s = new Student();
+        s.compute();
+    }
+    int id = 1000;
+    void compute(){
+        System.out.println(id);
+    }    
+}
