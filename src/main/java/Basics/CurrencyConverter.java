c582340596dbac2578d61b5353cde305d87cc662
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-01-19 05:19:07 +0000

    assigment on printing currencies completed

--- src/main/java/Basics/CurrencyConverter.java
+++ src/main/java/Basics/CurrencyConverter.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package Basics;
-
 /**
  *
  * @author HP
  */
 public class CurrencyConverter {
-    
+    int rupee, dirham, real, chilean_peso, mexican_peso, _yen, $australian, dollar, Rupee;
+    public static void main(String[] args){
+        CurrencyConverter cc = new CurrencyConverter();
+        cc.printCurrencies();
+    }
+    public CurrencyConverter(){
+        rupee=63;
+        dirham = 3;
+        real = 3;
+        chilean_peso = 595;
+        mexican_peso = 18;
+        _yen = 107;
+        $australian = 2;
+        Rupee = 63;
+        dollar = 1;
+    }
+    void printCurrencies(){
+        System.out.println("rupee: "+rupee);
+        System.out.println("dirham: "+ dirham);
+        System.out.println("real: "+real);
+        System.out.println("chilean_peso: "+chilean_peso);
+        System.out.println("mexican_peso: "+mexican_peso);
+        System.out.println("_yen: "+_yen);
+        System.out.println("$australian: "+$australian);
+        System.out.println("Rupee: "+Rupee);
+        System.out.println("dollar: "+dollar);    
+    }       
 }
--- src/main/java/Basics/Student.java
+++ src/main/java/Basics/Student.java
@@ -12,13 +12,22 @@ package Basics;
 public class Student {
     //Class and static variables are optional in declaration, int id; will also work fine
     //default value of int will be 0. If the variable is undeclared, the default value is updates to the variable
+    //Integer literals : int literals, long literal
     int id = 1000;
+    byte age = 18; //[-128,127] Possible lossy conversion from x to y if not coming inside the variable
+    short rank = 10123;
+    //_ can be used to increase the readability. This facility is Java7 and above. Cannot be used in integer literals
+    long phone = 945_6647_895L; //L cannot be assigned to any other integer types. Long only to long
     /*int x;
     x=10; //this declaration is not possible in java*/
     
     public static void main(String[] args){
         Student s = new Student();
         s.compute();
+        //Integer Boxed Primitive. Every primitive types have boxed primitive
+        int minValue= Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("Min" + minValue+" Max " +maxValue);
     }
 
     void compute(){
@@ -27,6 +36,8 @@ public class Student {
         int nextID =id+1;
         System.out.println(id);
         System.out.println(nextID);
-        System.out.println("The program ran succesfully");
+        System.out.println("Age is " +age);
+        System.out.println("Rank is" +rank);
+        System.out.println("Phone is " +phone);
     }    
 }
